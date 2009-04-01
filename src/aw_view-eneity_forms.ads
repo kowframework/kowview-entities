ea3dc0f6d7d768dc43c21831bfd05f43488c4d63
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2009-04-01 23:23:37 +0000

    lol
    git-svn-id: http://dev.adaworks.net/svn/trunk/awview-entity_forms@664 115cb82a-aa27-0410-86a0-a2651309cc67

--- src/aw_view-eneity_forms.ads
+++ src/aw_view-eneity_forms.ads
@@ -7,22 +7,40 @@ with Aw_View;
 generic
 	type Entity_Type is new Aw_Ent.Entity_Type with private;
 	Component_Name	: constant String;
-	Template_Name	: constant String;
 package Aw_View.Forms is
 
+	------------------------
+	-- The FORM Component --
+	------------------------
 
-	type Component_Type is new Aw_View.Component_Type;
+	type Component_Type is new Aw_View.Component_Interface with private;
 
 
-	-- módulos:
+
+
+
+	overriding
+	procedure Initialize(
+			Component	: in out Component_Type;
+			Component_Name	: in     String;
+			Config		: in out Aw_Config.Config_File
+		);
+	-- initialize this component
+	
+
+	overriding
+	function Create_Instance(
+			Component	: in Component_Type;
+			Module_Name	: in String;
+			Config		: in Aw_Config.Config_File
+		) return Aw_View.Module_Instance_Interface'Class;
+	-- create a module instance by given name
+	-- Available module:
+	-- 	view
 	-- 	edit_form
 	-- 	insert_form
-	--	display_form
-	--	list_elements_form
-	--	(...)
-	--
-	--
-	--	Serviços? AJAX!
+	
+
 
 
 
