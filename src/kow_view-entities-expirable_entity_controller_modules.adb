c3b87cbd814c4ecd397c498a2c95cf501b5b1d81
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2011-07-14 17:56:37 -0300

    lots of work done on the expirable entity controller modules package, but still lots to do... not even compilling right now

---
+++ src/kow_view-entities-expirable_entity_controller_modules.adb
@@ -0,0 +1,229 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                          KOW Framework :: View                           --
+--                                                                          --
+--                              KOW Framework                               --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--               Copyright (C) 2007-2011, KOW Framework Project             --
+--                                                                          --
+--                                                                          --
+-- KOWView is free software; you can redistribute it  and/or modify it under--
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. KOWView is distributed in the hope that it will be useful,but WITH---
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with KOWView; see file COPYING.  If not, write--
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+pragma License( GPL );
+
+
+
+------------------------------------------------------------------------------
+--                                                                          --
+--  This package provides a module implementation in the spirit of the enti---
+-- ty module type for handling expirable entity controllers                 --
+--                                                                          --
+------------------------------------------------------------------------------
+
+
+--------------
+-- Ada 2005 --
+--------------
+with Ada.Strings.Unbounded;			use Ada.Strings.Unbounded;
+with Ada.Tags;
+
+-------------------
+-- KOW Framework --
+-------------------
+with KOW_Ent;
+with KOW_Sec;
+with KOW_View.Entities.Modules;
+
+
+---------
+-- AWS --
+---------
+with AWS.Parameters;
+with AWS.Status;
+
+
+
+package body KOW_View.Entities.Expirable_Entity_Controller_Modules is
+
+
+	---------------------
+	-- The Module Type --
+	---------------------
+
+
+	Entity_Tag : constant Unbounded_String := To_Unbounded_String( Ada.Tags.Expanded_Name( Entity_Type'Tag ) );
+	overriding
+	procedure Initialize_Request(
+				Module	: in out Lifetime_Handler_Module_Type;
+				Request	: in     AWS.Status.Data;
+				Config	: in out KOW_Config.Config_File
+			) is
+		-- call entity_module_type's setup
+		-- Entity_Tag is always set to Entity_Type'Tag;
+	begin
+		KOW_View.Entities.Modules.Initialize_Request(
+					Module	=> KOW_View.Entities.Modules.Entity_Module_Type( Module ),
+					Request	=> Request,
+					Config	=> Config
+				);
+
+		Module.Entity_Tag := Entity_Tag;
+		Module.Style := Small_Rendering;
+	end Initialize_Request;
+
+	overriding
+	procedure Process_Body(
+				Module	: in out Lifetime_Handler_Module_Type;
+				Request	: in     AWS.Status.Data;
+				Output	:    out Unbounded_String
+			) is
+		-- always render list
+	begin
+		Module.Style := Small_Rendering;
+		Render_List(
+				Module	=> Lifetime_Handler_Module_Type'Class( Module ),
+				Request	=> Request,
+				Output	=> Output
+			);
+	end Process_Body;
+
+
+
+	overriding
+	procedure Process_Json_Request(
+				Module	: in out Lifetime_Handler_Module_Type;
+				Request	: in     AWS.Status.Data;
+				Response:    out KOW_Lib.Json.Object_Type
+			) is
+		-- act upon actions from Lifetime_Action_Type
+		Obj : KOW_Lib.Json.Object_Type;
+
+
+		function Action return Lifetime_Action_Type is
+			Act : constant String := AWS.Parameters.Get( P, "action" );
+		begin
+			if Act = "" then
+				raise PROGRAM_ERROR with "Seems like the developer forgot to pass the action parameter...";
+			else
+				return Lifetime_Action_Type'Value( Act );
+			end if;
+		exception
+			when constraint_error =>
+				raise PROGRAM_ERROR with "Invalid value for action: " & Act;
+		end Action;
+	begin
+		case Action is
+			when Expire_Entity =>
+				Controllers.Expire( Get_Entity );
+			when Validate_Entity =>
+				Controllers.Validate( Get_Entity );
+			when Store_Validation_Period =>
+			when Render_Form =>
+				declare
+					Buffer : Unbounded_String;
+				begin
+					Module.Style := Big_Edit_Rendering;
+
+					Render_View(
+							Module	=> Lifetime_Handler_Module_Type'Class( Module ),
+							Request	=> Request,
+							Entity	=> Get_Validatinon_Entity(
+											Module	=> Lifetime_Handler_Module_Type'Class( Module ),
+											Request	=> Request
+										),
+							Output	=> Buffer
+						);
+					KOW_Lib.Json.Set( Obj, "formHTML", Buffer );
+				end;
+		end case;
+
+
+		
+
+		KOW_Lib.Json.Set( Obj, "action", Lifetime_Action_Type'Image( Action ) );
+		Response := Obj;
+	end Process_Json_Request;
+
+
+	---------------
+	-- Rendering --
+	---------------
+	overriding
+	procedure Render_View_Buttons(
+				Module	: in out Lifetime_Handler_Module_Type;
+				Request	: in     AWS.Status.Data;
+				Entity	: in     KOW_Ent.Entity_Type'Class;
+				Output	:    out Unbounded_String
+			);
+	-- render the correct buttons for the form
+
+
+	-----------------
+	-- New Methods --
+	-----------------
+
+	function Get_Validation_Extensions(
+				Module	: in Lifetime_Handler_Module_Type;
+				Request	: in AWS.Status.Data
+			) return Tag_Array is
+		-- override this method in case you want to enable your user to
+		-- create
+		--
+		-- default: return an empty array
+		Empty : Tag_Array( 2 .. 1 );
+	begin
+		return Empty;
+	end Get_Validation_Extensions;
+
+
+	function Get_Validation_Entity(
+				Module	: in Lifetime_Handler_Module_Type;
+				Request	: in AWS.Status.Data
+			) return Controllers.Validation_Entity'Class is
+		-- if entity_id is set, get the validation entity for this entity
+		-- or else, allocate a new entity by the validation_entity_tag parameter
+		Entity_ID	: constant Integer := Get_Entity_Id( Lifetime_Handler_Module_Type'Class( Module ), Request );
+		The_Tag 	: constant String := AWS.Parameters.Get( AWS.Status.Parameters( Request ), "validation_entity_tag" );
+	begin
+		if Entity_Id /= -1 then
+			declare
+				Entity : Entity_Type;
+			begin
+				KOW_Ent.Load( Entity, Natural( Entity_Id ) );
+				Controllers.Get_Validation( Entity );
+			end;
+		elsif The_Tag /= "" and The_Tag /= Ada.Tags.Extended_Name( Entity_Type'Tag ) then
+			declare
+				Available : Get_Validation_Extensions( Lifetime_Handler_Module_Type'Class( Module ), Request );
+			begin
+				for i in Available'Range loop
+					if The_Tag = Available( i ) then
+						return KOW_Ent.New_Entity( The_Tag );
+					end if;
+				end loop;
+
+				raise KOW_Sec.Access_Denied with "Sorry, but you don't have permission to create this entity";
+			end;
+		else
+			declare
+				The_Entity : Entity_Type;
+			begin
+				return The_Entity;
+			end;
+		end if;
+
+	end Get_Validation_Entity;
+
+end KOW_View.Entities.Expirable_Entity_Controller_Modules;
---
+++ src/kow_view-entities-expirable_entity_controller_modules.ads
@@ -0,0 +1,152 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                          KOW Framework :: View                           --
+--                                                                          --
+--                              KOW Framework                               --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--               Copyright (C) 2007-2011, KOW Framework Project             --
+--                                                                          --
+--                                                                          --
+-- KOWView is free software; you can redistribute it  and/or modify it under--
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. KOWView is distributed in the hope that it will be useful,but WITH---
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with KOWView; see file COPYING.  If not, write--
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+pragma License( GPL );
+
+
+
+------------------------------------------------------------------------------
+--                                                                          --
+--  This package provides a module implementation in the spirit of the enti---
+-- ty module type for handling expirable entity controllers                 --
+--                                                                          --
+------------------------------------------------------------------------------
+
+
+--------------
+-- Ada 2005 --
+--------------
+with Ada.Strings.Unbounded;		use Ada.Strings.Unbounded;
+with Ada.Tags;
+
+-------------------
+-- KOW Framework --
+-------------------
+with KOW_Ent;
+with KOW_Ent.Expirable_Entity_Controllers;
+with KOW_View.Entities.Modules;
+
+
+---------
+-- AWS --
+---------
+with AWS.Status;
+
+
+generic
+	type Entity_Type is new KOW_Ent.Entity_Type with private;
+	with package Controllers is new KOW_Ent.Expirable_Entity_Controllers( Entity_Type );
+package KOW_View.Entities.Expirable_Entity_Controller_Modules is
+
+
+	------------------------------------
+	-- HTTP Request Control Variables --
+	------------------------------------
+
+	type Lifetime_Action_Type is (
+	-- the lifetime action type is used by the json callback to identify
+	-- what to do.
+			Expire_Entity,			-- expirate the entity _NOW_
+			Validate_Entity,		-- validate the entity _NOW_
+			Store_Validation_Period,	-- store (insert a new or update an existing) a validation period
+			Render_Form			-- render the entity form; if the validation_id is suplied, load the entity before
+		);
+	
+	type View_entity_Type is(
+			Valid_Entities,
+			Every_Entity
+		);
+
+	---------------------
+	-- The Module Type --
+	---------------------
+
+
+	type Lifetime_Handler_Module_Type is abstract new KOW_View.Entities.Modules.Entity_Module_Type with null record;
+	-- this module will render a list of entities, giving the ability to expire/validate and revalidate entities
+
+	overriding
+	procedure Initialize_Request(
+				Module	: in out Lifetime_Handler_Module_Type;
+				Request	: in     AWS.Status.Data;
+				Config	: in out KOW_Config.Config_File
+			);
+	-- call Entity_Module_Type's setup
+	-- Entity_Tag is always set to Entity_Type'Tag;
+
+
+	overriding
+	procedure Process_Body(
+				Module	: in out Lifetime_Handler_Module_Type;
+				Request	: in     AWS.Status.Data;
+				Output	:    out Unbounded_String
+			);
+	-- always render list
+	
+	overriding
+	procedure Process_Json_Request(
+				Module	: in out Lifetime_Handler_Module_Type;
+				Request	: in     AWS.Status.Data;
+				Response:    out KOW_Lib.Json.Object_Type
+			);
+	-- act upon actions from Lifetime_Action_Type
+
+
+
+	---------------
+	-- Rendering --
+	---------------
+	overriding
+	procedure Render_View_Buttons(
+				Module	: in out Lifetime_Handler_Module_Type;
+				Request	: in     AWS.Status.Data;
+				Entity	: in     KOW_Ent.Entity_Type'Class;
+				Output	:    out Unbounded_String
+			);
+	-- render the correct buttons for the form
+
+
+	-----------------
+	-- New Methods --
+	-----------------
+
+
+	type Tag_Array is array( Positive range <> ) of Ada.Tags.Tag;
+
+	function Get_Validation_Extensions(
+				Module	: in Lifetime_Handler_Module_Type;
+				Request	: in AWS.Status.Data
+			) return Tag_Array;
+	-- override this method in case you want to enable your user to
+	-- create
+	--
+	-- default: return an empty array
+
+	function Get_Validation_Entity(
+				Module	: in Lifetime_Handler_Module_Type;
+				Request	: in AWS.Status.Data
+			) return Controllers.Validation_Entity'Class;
+	-- if entity_id is set, get the validation entity for this entity
+	-- or else, allocate a new entity by the validation_entity_tag parameter
+
+end KOW_View.Entities.Expirable_Entity_Controller_Modules;
