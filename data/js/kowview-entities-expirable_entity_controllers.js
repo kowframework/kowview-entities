563c33578796151a7fb7baa98e2558f678cb919f
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2011-08-09 14:12:02 -0300

    adding script for expirable entity controllers

---
+++ data/js/kowview-entities-expirable_entity_controllers.js
@@ -0,0 +1,35 @@
+/**
+ * Script for the expirable entity controller module types
+ */
+
+
+
+
+
+var kowview.entities.expirable_entity_controllers = new Object();
+
+
+dojo.addOnLoad(function(){console.log("oie!")});
+
+
+
+
+/*************************************/
+/* Driver Functions called from Ada */
+/***********************************/
+
+
+/**
+ * Update the module listing
+ */
+kowview.entities.expirable_entity_controllers.updateList = function( select ){
+	console.dir(select);
+}
+
+
+/**
+ * Initialize a item, building the menu
+ */
+kowview.entities.expirable_entity_controllers.initializeItem = function( moduleId, itemId, isValid ) {
+	console.log( "initializing item" + itemId );
+}
